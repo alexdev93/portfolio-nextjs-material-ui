275e86ec8b7c6334fc103b061247fef16a1bb4d5
alexdev <alemayehu@addissoftware.com>
2024-10-26 14:28:22 +0300

    change profile photo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
-'use client'
-import React from 'react';
+"use client";
+import React from "react";
 import {
   Header,
   HeaderContent,
@@ -9,11 +9,11 @@ import {
   RightHeader,
   Name,
   Description,
-  MainButton
-} from '../styles/HomeStyles';
-import Image from 'next/image';
-import AnimatedBox from '../components/AnimatedBox';
-import useIsMobile from '../utils/mediaQueries';
+  MainButton,
+} from "../styles/HomeStyles";
+import Image from "next/image";
+import AnimatedBox from "../components/AnimatedBox";
+import useIsMobile from "../utils/mediaQueries";
 
 const HomeHeader = () => {
   const isMobile = useIsMobile();
@@ -24,31 +24,37 @@ const HomeHeader = () => {
           <HShape className="h-shape"></HShape>
           <ImageContainer>
             <Image
-              src='/photo_2024-07-16_06-27-39.jpg'
+              src="/portofolio.jpg"
               alt=""
-              layout='fill' // Fill the container
-              objectFit='cover'
+              layout="fill" // Fill the container
+              objectFit="cover"
             />
           </ImageContainer>
         </LeftHeader>
         <RightHeader className="right-header">
           <AnimatedBox variant="fadeInUp">
             <Name className="name">
-              Hi, I&apos;m <span>Alemayehu Mekonen.</span>
-              A Web Developer.
+              Hi, I&apos;m <span>Alemayehu Mekonen.</span>A Web Developer.
             </Name>
           </AnimatedBox>
           <AnimatedBox variant="fadeIn">
-            <Description style={{ fontStyle: 'italic' }}>
+            <Description style={{ fontStyle: "italic" }}>
               &quot;Bringing your online vision to life,
               <span className="pixel">one pixel at a time</span>&quot;
-              emphasizes my dedication to creating customized and visually appealing website that accurately reflect the brand and
-              goals of each client.
+              emphasizes my dedication to creating customized and visually
+              appealing website that accurately reflect the brand and goals of
+              each client.
             </Description>
           </AnimatedBox>
-          <MainButton href="./ALEMAYEHU MEKONEN-2.pdf" className="main-btn" download>
+          <MainButton
+            href="./ALEMAYEHU MEKONEN-2.pdf"
+            className="main-btn"
+            download
+          >
             <span className="btn-text">Download CV</span>
-            <span className="btn-icon"><i className="fas fa-download"></i></span>
+            <span className="btn-icon">
+              <i className="fas fa-download"></i>
+            </span>
           </MainButton>
         </RightHeader>
       </HeaderContent>
